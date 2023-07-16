d97d6fcad10902501845e8be6111faf345b038aa
carsonjames1125 <tarheelkid44@yahoo.com>
2023-07-16 14:26:04 -0400

    finished the code for the models section, moving on to the routes section

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,33 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+//need to add foreign keys to each section 
 
-// Categories have many Products
+// Products belongsTo Category **
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Categories have many Products **
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+// Products belongToMany Tags (through ProductTag) **
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+// Tags belongToMany Products (through ProductTag) **
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
